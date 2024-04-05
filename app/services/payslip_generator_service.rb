d24c5060aca9d19131a0f5c1315159f72c6dd66e
Gan Yi Zhong <ganyizhong@gmail.com>
2024-04-05 12:55:56 +0800

    requirement 2

---
+++ app/services/payslip_generator_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class PayslipGeneratorService
+  def initialize(name, annual_salary)
+    @name = name
+    @annual_salary = annual_salary.to_f
+  end
+
+  def call
+    {
+      employee_name: @name,
+      gross_monthly_income: format('%.2f', monthly_income(@annual_salary)),
+      monthly_income_tax: format('%.2f', monthly_income_tax(@annual_salary)),
+      net_monthly_income: format('%.2f', net_monthly_income(@annual_salary))
+    }
+  end
+end
